050130953db2224bd84f4be444dee80fd7d99db6
roshangm1 <roosan.gm@gmail.com>
2016-07-10 09:13:52 +0545

    commmmmmmit

--- RateyourISP/app/src/main/java/roshan/info/np/rateyourisp/activities/AboutUs.java
+++ RateyourISP/app/src/main/java/roshan/info/np/rateyourisp/activities/AboutUs.java
@@ -1,31 +1,61 @@
 package roshan.info.np.rateyourisp.activities;
 
+import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnMenuTabClickListener;
 
 import roshan.info.np.rateyourisp.R;
 
-public class AboutUs extends AppCompatActivity {
+public class AboutUs extends AppCompatActivity implements View.OnClickListener{
 
-    BottomBar bottomBar;
+    private LinearLayout about;
+    private RelativeLayout product;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
 
-        bottomBar = BottomBar.attach(this,savedInstanceState);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_about);
+        setSupportActionBar(toolbar);
+        setTitle("Who are we ?");
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.setNavigationIcon(R.drawable.close);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+
+        about = (LinearLayout) findViewById(R.id.about);
+        product = (RelativeLayout) findViewById(R.id.products);
+
+        BottomBar bottomBar = BottomBar.attach(this, savedInstanceState);
         bottomBar.setItems(R.menu.bottom_menu);
         bottomBar.setOnMenuTabClickListener(new OnMenuTabClickListener() {
             @Override
             public void onMenuTabSelected(@IdRes int menuItemId) {
                 if(menuItemId==R.id.about_us) {
-
+                    about.setVisibility(View.VISIBLE);
+                    product.setVisibility(View.GONE);
                 } else {
 
+                    product.setVisibility(View.VISIBLE);
+                    about.setVisibility(View.GONE);
                 }
             }
 
@@ -37,5 +67,10 @@ public class AboutUs extends AppCompatActivity {
 
         bottomBar.mapColorForTab(0, "#f44336");
         bottomBar.mapColorForTab(1, "#ff4081");
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
